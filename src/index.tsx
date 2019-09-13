d7e72db3917eb1b5f32daa7663a597563ea46b24
Meetingpackage dev <meetingpackagedev@Meetingpackages-MacBook-Pro.local>
2019-09-13 12:13:53 +0300

    try polyfill

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import ReduxRoot from './ReduxRoot';
+import 'react-app-polyfill/ie11'
 
-const rootEl = document.getElementById('root');
-ReactDOM.render(<ReduxRoot />, rootEl);
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import ReduxRoot from './ReduxRoot'
+
+const rootEl = document.getElementById('root')
+ReactDOM.render(<ReduxRoot />, rootEl)
 
 // comment in for PWA with service worker in production mode
 // registerServiceWorker();
