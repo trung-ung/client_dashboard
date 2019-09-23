d7c4d8f33a9ffdb5326fb4af1a47750cee83d42e
Trung Ung <trung.ung@meetingpackage.com>
2019-09-23 16:44:39 +0300

    add style to day speaker using additional theme provider

--- src/views/Dashboard/components/DayRangeFilter/DayRangeFilterMobile.js
+++ src/views/Dashboard/components/DayRangeFilter/DayRangeFilterMobile.js
@@ -1,4 +1,4 @@
-import { Grid } from '@material-ui/core'
+import { Grid, Typography } from '@material-ui/core'
 //import moment from 'moment'
 import React, { useEffect, useRef, useState } from 'react'
 import DayPickerInput from 'react-day-picker/DayPickerInput'
@@ -15,6 +15,27 @@ import subMonths from 'date-fns/subMonths'
 import { isToday, format } from 'date-fns'
 import YearMonthForm from './YearMonthForm'
 import { DatePicker } from '@material-ui/pickers'
+import Toolbar from '@material-ui/core/Toolbar'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { colors } from '@material-ui/core'
+import { ThemeProvider } from '@material-ui/styles'
+const white = '#FFFFFF'
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      contrastText: white,
+      dark: '#003366',
+      main: '#0054a8',
+      light: '#0072e5'
+    },
+    secondary: {
+      contrastText: white,
+      dark: colors.blue[900],
+      main: colors.blue['A400'],
+      light: colors.blue['A400']
+    }
+  }
+})
 
 export const DayRangeFilter = props => {
   const from = useSelector(state => state.filter.from)
@@ -53,32 +74,43 @@ export const DayRangeFilter = props => {
   return (
     <>
       <Grid item xs={12} sm={6} md={3} lg={3} xl={3}>
-        <DatePicker
-          autoOk
-          label={fromText}
-          format="dd/MM/yyyy"
-          clearable
-          inputVariant="outlined"
-          disableFuture
-          value={from}
-          onChange={handleFromChange}
-          style={{ width: '100%' }}
-        />
+        <ThemeProvider theme={theme}>
+          <DatePicker
+            autoOk
+            label={fromText}
+            format="dd/MM/yyyy"
+            inputVariant="outlined"
+            disableFuture
+            value={from}
+            onChange={handleFromChange}
+            style={{ width: '100%' }}
+          />
+        </ThemeProvider>
       </Grid>
 
       <Grid item xs={12} sm={6} md={3} lg={3} xl={3}>
-        <DatePicker
-          autoOk
-          label={toText}
-          format="dd/MM/yyyy"
-          inputVariant="outlined"
-          clearable
-          minDate={new Date(from)}
-          disableFuture
-          value={to}
-          onChange={handleToChange}
-          style={{ width: '100%' }}
-        />
+        <ThemeProvider theme={theme}>
+          <DatePicker
+            // ToolbarComponent={props => {
+            //   console.log('propsssssss', props)
+            //   return (
+            //     <Toolbar {...props}>
+            //       <Typography></Typography>
+            //       <Typography></Typography>
+            //     </Toolbar>
+            //   )
+            // }}
+            autoOk
+            label={toText}
+            format="dd/MM/yyyy"
+            inputVariant="outlined"
+            minDate={new Date(from)}
+            disableFuture
+            value={to}
+            onChange={handleToChange}
+            style={{ width: '100%' }}
+          />
+        </ThemeProvider>
       </Grid>
     </>
   )
